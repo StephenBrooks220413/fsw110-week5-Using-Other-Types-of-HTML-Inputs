266c7272088fefa0a48cdc0a1f6edc95ed2a92e2
stephenbrooks40 <stephenbrooks714@gmail.com>
2021-06-01 06:37:40 -0700

    update

---
+++ index.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    var fName = document.getElementById("firstName").value;
+    var lName = document.getElementById("lastName").value;
+    var Choice = document.getElementById("decision").value;
+    var age = document.getElementById("age").value;
+    var answer = document.getElementById("answer").value;
+    var options = document.getElementById("options").value;
+    var food = document.getElementById("food").value;
+    alert(fName + " " + lName + " " + Choice + " " + age + " " + answer + " " + options + " " + food);
+}
